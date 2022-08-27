5f1d9259464bf93e4bbb17facf2f6fdb3e3719b9
AlexandrVladlenovich <Mystafin_Alexandr@mail.ru>
2022-08-27 22:24:55 +0500

    add Zadacha041_Bolshe0

---
+++ Zadacha041_Bolshe0/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.Write("Введите числа через пробел: ");
+
+int[] array = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+int Numbers(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+Console.WriteLine($"Чисел больше ноля: {Numbers(array)}");
